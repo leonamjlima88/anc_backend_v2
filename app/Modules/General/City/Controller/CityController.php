0242439ac89a4034c0e660b70ca208305320e2ff
Leonam <leonam@puzl.place>
2022-08-28 16:49:48 -0300

    .

--- app/Modules/General/City/Controller/CityController.php
+++ app/Modules/General/City/Controller/CityController.php
@@ -20,8 +20,7 @@ class CityController extends Controller
 
   public function index()
   {
-    $dataResult = new CityIndexResource($this->service->index());
-    
+    $dataResult = new CityIndexResource($this->service->index());    
     return Res::success($dataResult);
   }
 
